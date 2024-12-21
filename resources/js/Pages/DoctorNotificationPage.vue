c142ae6abb0fb9b2eff2529d85b8be9512fc3aa0
Francesca Maries P. Buguis <fpbuguis@up.edu.ph>
2024-12-21 10:34:33 +0800

    Update DoctorNotificationPage.vue

--- resources/js/Pages/DoctorNotificationPage.vue
+++ resources/js/Pages/DoctorNotificationPage.vue
@@ -156,7 +156,7 @@ const unreadMessageCount = computed(() => {
 const jsonString = ref("");
 const pdfUrl = ref(null);
 // const baseUrl = 'http://127.0.0.1:8000/storage/';
-const baseUrl = 'https://css-d-production.up.railway.app/storage/';
+const baseUrl = 'https://css-d-production.up.railway.app/storage/' ;
 // const baseUrl = `${import.meta.env.APP_URL}/storage/`;
 
 const parseJson = (jsonString) => {
